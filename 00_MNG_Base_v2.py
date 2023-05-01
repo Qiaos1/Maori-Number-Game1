b108e5d0026e6870a3c792dbbcab46e221ba308c
Qiaos1 <qiaos@middleton.school.nz>
2023-05-02 09:55:05 +1200

    Hi Elme

--- 00_MNG_Base_v2.py
+++ 00_MNG_Base_v2.py
@@ -1,5 +1,43 @@
-"""MNG base component v1
-Components added after they have been tested and created"""
+"""MNG base component v2
+Adds Component 3 and 4 to the base component"""
+
+
+import random
+
+
+# Māori numbers from one to ten
+māori_number = {
+    1: "tahi",
+    2: "rua",
+    3: "toru",
+    4: "wha",
+    5: "rima",
+    6: "ono",
+    7: "whitu",
+    8: "waru",
+    9: "iwa",
+    10: "tekau"}
+
+
+# Function to generate random number between 1 and 10
+def generate_number():
+    return random.randint(1, 10)
+
+
+# Function to play the Māori Number Game
+def māori_game():
+    while True:
+        number = generate_number()
+        print(number)
+        answer = input(f"What is the Maori name for {number}? (Type Exit to"
+                       f" stop playing)")
+        if answer.lower() == "exit":
+            break
+        elif answer.lower() == māori_number[number]:
+            print(f"Congratulations! You are correct!")
+        else:
+            print(f"Sorry, you are incorrect. The correct answer is"
+                  f" {māori_number[number]}")
 
 
 # Function for question
@@ -45,3 +83,4 @@ if played_before == "No":
     instructions()
 else:
     print("Game continues")
+māori_game()
