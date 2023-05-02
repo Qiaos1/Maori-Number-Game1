e71e6817f47fecf2275408455e8870f73749e974
Qiaos1 <qiaos@middleton.school.nz>
2023-05-02 18:29:23 +1200

    Sarah was here again

--- 05_Rounds_v1.py
+++ 05_Rounds_v1.py
@@ -1,1 +1,17 @@
-"""Component 5 (Rounds) v1"""
+"""Component 5 (Rounds) v1
+Generates rounds and asks user whether they want to carry on playing or not"""
+
+# Ask user whether they want to play again or not
+rounds = input("Would you like to play another round?")
+
+# If they say yes, output Game Continues
+if rounds == "yes":
+    print("Game Continues")
+
+# If they say no, end game
+elif rounds == "no":
+    print("Thank you for playing!")
+
+# Otherwise show error
+else:
+    print("Sorry that wasn't a option")
---
+++ 05_Rounds_v2.py
@@ -0,0 +1,3 @@
+"""Component 5 (Rounds) v1
+Solves the problem of upper cases and also allows abbreviations.
+Also states user choice at the end"""
