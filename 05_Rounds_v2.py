6ac107c018468352f8c5142e5a7bf853d82b4856
Qiaos1 <qiaos@middleton.school.nz>
2023-05-03 14:05:21 +1200

    Eleasha was here

--- 05_Rounds_v2.py
+++ 05_Rounds_v2.py
@@ -1,3 +1,21 @@
-"""Component 5 (Rounds) v1
+"""Component 5 (Rounds) v2
 Solves the problem of upper cases and also allows abbreviations.
 Also states user choice at the end"""
+
+# Ask user whether they want to play again or not
+rounds = input("Would you like to play another round? ")
+
+# Convert the user input to lowercase
+rounds = rounds.lower()
+
+# If they say yes, output Game Continues
+if rounds == "yes":
+    print("Game Continues")
+
+# If they say no, end game
+elif rounds == "no":
+    print("Thank you for playing!")
+
+# Otherwise show error
+else:
+    print("Sorry that wasn't an option")
